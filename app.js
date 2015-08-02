67a489fad9a672887ddcf4394bf113e06913f410
Ricardo de la Concepcion Leal <rconleau@gmail.com>
2015-08-02 18:30:00 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var path = require('path');
+
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var partials = require('express-partials');
+var methodOverride = require('method-override');
+
+var routes = require('./routes/index');
+
+var app = express();
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(partials());
+
+
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// Manejador de errores
+
+
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err, 
+            errors: []
+        });
+    });
+}
+
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}, 
+        errors: []
+    });
+});
+
+
+module.exports = app;
