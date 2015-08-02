a2732007558e1354e3d125638d5555997831aa93
Ricardo de la Concepcion Leal <rconleau@gmail.com>
2015-08-02 20:17:34 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var path = require('path');
-
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -12,14 +10,8 @@ var routes = require('./routes/index');
 
 var app = express();
 
-
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(partials());
-
-
-app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
